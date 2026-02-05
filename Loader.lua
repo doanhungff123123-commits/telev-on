b589a128aa882e6bfadbaed8a6ed0497c74a2017
doanhungff123123-commits <doanhungff123123@gmail.com>
2026-02-05 19:42:30 +0700

    Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,20 +1,17 @@
--- Loader.lua
--- File này sẽ load script từ GitHub
-
-local ScriptURL = "https://raw.githubusercontent.com/hungdao/tele/main/HungDao9999.lua"
+local ScriptURL = "https://raw.githubusercontent.com/doanhungf123123/commits/main/HungDao9999.lua"
 
 local success, result = pcall(function()
     local scriptContent = game:HttpGet(ScriptURL)
     local scriptFunction = loadstring(scriptContent)
     
     if scriptFunction then
-        print("✅ Đã tải script thành công!")
-        scriptFunction()() -- Gọi function được return từ script
+        print("Script loaded successfully!")
+        scriptFunction()()
     else
-        warn("❌ Không thể load script!")
+        warn("Cannot load script!")
     end
 end)
 
 if not success then
-    warn("❌ Lỗi khi tải script: " .. tostring(result))
+    warn("Error loading script: " .. tostring(result))
 end
